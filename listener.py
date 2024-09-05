a18f905406efd22a4eaa16ce6b35fda19230a5a9
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2024-09-05 16:59:40 -0400

    Use logger instead of print statements

--- listener.py
+++ listener.py
@@ -13,6 +13,7 @@ from azure.storage.blob import BlobServiceClient
 import zipfile
 import requests
 import shutil
+import logging
 from typing import Optional, Callable
 
 class ScanResult:
@@ -93,13 +94,15 @@ def upload_to_blob_storage(data: str, container_name: str, blob_name: str) -> No
 
 
 def listen_for_messages():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
     topicName = "WACSEvent"
     resultTopicName = "LintingResult"
     subscriptionName = "LarrysScripts"
     connstr = os.environ.get("Azure_ServiceBus_Connection_String")
     output_prefix = os.environ.get("OUTPUT_PREFIX")
     if not connstr:
-        print("No Service Bus connection string found")
+        logger.error("No Service Bus connection string found")
         exit(1)
     with ServiceBusClient.from_connection_string(connstr) as client:
         with client.get_subscription_receiver(topicName, subscriptionName, uamqp_transport=True) as receiver:
@@ -111,12 +114,12 @@ def listen_for_messages():
                 repo = parsed["Repo"]
                 id = parsed["RepoId"]
 
-                print(f"Scannning {user}/{repo}")
+                logger.info(f"Scanning {user}/{repo}")
 
                 repourl = f"{parsedUrl.scheme}://{parsedUrl.netloc}/api/v1/repos/{user}/{repo}/archive/{defaultBranch}.zip"
                 # Get a temporary dir
-                logger = ResultsListener()
-                logger.progress_callback = lambda msg: (receiver.renew_message_lock(message), None)[1]
+                result_logger = ResultsListener()
+                result_logger.progress_callback = lambda msg: (receiver.renew_message_lock(message), None)[1]
                 with tempfile.TemporaryDirectory() as tempdir:
                     with tempfile.NamedTemporaryFile() as downloadFile:
                         response = requests.get(repourl, stream=True)
@@ -128,23 +131,16 @@ def listen_for_messages():
 
                     # Unzip repo file
                     try:
-                        scan_dir(tempdir, logger)
+                        scan_dir(tempdir, result_logger)
                     except Exception as e:
                         receiver.dead_letter_message(message, reason=f"Error scanning {user}/{repo}", error_description=str(e))
-                        print(f"Error scanning {user}/{repo}: {e}")
+                        logger.error(f"Error scanning {user}/{repo}: {e}")
                     finally:
                         shutil.rmtree(tempdir)
-                print("Results")
-                for book, chapters in logger.result.results.items():
-                    print(f"{book}")
-                    for chapter, verses in chapters.items():
-                        print(f"  {chapter}")
-                        for verse in verses:
-                            print(verse['message'])
-                print(f"Done scanning {user}/{repo}")
-                if len(logger.result.results) > 0:
+                logger.info(f"Scanning {user}/{repo} complete")
+                if len(result_logger.result.results) > 0:
                     output_path = f"{user}/{repo}.json"
-                    upload_to_blob_storage(logger.result.to_json(), "scan-results", output_path)
+                    upload_to_blob_storage(result_logger.result.to_json(), "scan-results", output_path)
                     uploaded_url = f"{output_prefix}/{output_path}"
                     with client.get_topic_sender(resultTopicName) as sender:
                         sender.send_messages(ServiceBusMessage(json.dumps({"User": user, "Repo": repo, "RepoId": id, "ResultsFileUrl": uploaded_url})))
